2bc9cd59fdde39dfbbf4d579df0bb3dc62678f4b
tylerhenniges <tylerhenniges@192.168.1.11>
2018-11-21 10:12:03 -0500

    Tyler: cleaned up comments on IDecoder

--- Assignment6/src/IDecoder.java
+++ Assignment6/src/IDecoder.java
@@ -1,7 +1,4 @@
 public interface IDecoder {
-	// Given the file path of the binary file, the decoder will read the file, decode the message and 
-	// print it to the console.
-	// Include code to handle the IOException.
 	/**
 	 * Given the file path of the binary file, the decoder will read the file, decode the message and 
 	 * print it to the console.
